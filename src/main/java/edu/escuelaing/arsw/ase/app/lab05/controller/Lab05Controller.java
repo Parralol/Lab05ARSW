a5fba21598041788fbc3186d0f13d18cbbe802d5
paralol <santinico36@hotmail.com>
2024-06-25 10:24:30 -0500

    final changes

--- src/main/java/edu/escuelaing/arsw/ase/app/lab05/controller/Lab05Controller.java
+++ src/main/java/edu/escuelaing/arsw/ase/app/lab05/controller/Lab05Controller.java
@@ -8,40 +8,67 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Lab05Controller {
-
+    int count = 0;
     @GetMapping("/prueba")
-    public String loadIndex(){
+    public String loadIndex() {
         return "prueba";
     }
 
     @GetMapping("/calculator")
     public String showCalculator() {
+        
         return "calculator";
     }
 
     @PostMapping("/calculate")
-    public String calculate(@RequestParam double number1,
-                            @RequestParam double number2,
-                            @RequestParam String operation,
-                            Model model) {
+    public String calculate(@RequestParam String number1,
+            Model model) {
         double result = 0;
+        count =0;
+        String operation = getOperation(number1);
         switch (operation) {
-            case "+":
-                result = number1 + number2;
+            case "\\+":
+                result = Double.parseDouble(number1.split(operation)[0]) + Double.parseDouble(number1.split(operation)[1]);
                 break;
-            case "-":
-                result = number1 - number2;
+            case "\\-":
+                result = Double.parseDouble(number1.split(operation)[0]) - Double.parseDouble(number1.split(operation)[1]);
                 break;
-            case "*":
-                result = number1 * number2;
+            case "\\*":
+                result = Double.parseDouble(number1.split(operation)[0]) * Double.parseDouble(number1.split(operation)[1]);
                 break;
-            case "/":
-                result = number1 / number2;
+            case "\\/":
+                result = Double.parseDouble(number1.split(operation)[0]) / Double.parseDouble(number1.split(operation)[1]);
                 break;
         }
         model.addAttribute("result", result);
-        model.addAttribute("number1", number1);
-        model.addAttribute("number2", number2);
+        return "calculator";
+    }
+
+    @PostMapping("/case")
+    public String cases(@RequestParam String number1,
+            @RequestParam String operation,
+            Model model) {
+        count++;
+        if(count >1){
+            model.addAttribute("number1", number1);
+            return "calculator";
+        } 
+        switch (operation) {
+
+            case "+":
+                model.addAttribute("number1", number1 + "+");
+                break;
+            case "-":
+                model.addAttribute("number1", number1 + "-");
+                break;
+            case "*":
+                model.addAttribute("number1", number1 + "/");
+                break;
+            case "/":
+                model.addAttribute("number1", number1 + "*");
+                break;
+        }
+
         return "calculator";
     }
 
@@ -50,6 +77,21 @@ public class Lab05Controller {
         model.addAttribute("result", 0);
         model.addAttribute("number1", 0);
         model.addAttribute("number2", 0);
+        count = 0;
         return "calculator";
+    }
+
+    public String getOperation(String op){
+        String res = "";
+        if(op.indexOf('+') > 0){
+            res = "\\+";
+        }else if(op.indexOf('-') > 0){
+            res = "\\-";
+        }else if(op.indexOf('*') > 0){
+            res = "\\*";
+        }else if(op.indexOf('/') > 0){
+            res = "\\/";
+        }
+        return res;
     }
 }
